b39b13499811e80239f0719a6b46e0c321020cb4
ynazaruk <nazaruk.ya@gmail.com>
2024-05-06 21:24:56 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,6 +2,10 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const myInput = document.querySelector('#datetime-picker');
+const startBTn = document.querySelector('button[data-start]');
+// console.log(startBTn);
+let userSelectedDate;
+
 const fp = flatpickr(myInput, {
   enableTime: true,
   time_24hr: true,
@@ -9,7 +13,10 @@ const fp = flatpickr(myInput, {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      return alert('Please choose a date in the future');
+    } else {
+      userSelectedDate = selectedDates[0];
+    }
   },
 });
-
-let userSelectedDate;
