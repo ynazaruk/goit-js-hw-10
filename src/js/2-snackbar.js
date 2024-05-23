2a24c916c45dd312c5b90222f141451f5cc02448
ynazaruk <nazaruk.ya@gmail.com>
2024-05-23 22:57:23 +0300

    1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,9 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
